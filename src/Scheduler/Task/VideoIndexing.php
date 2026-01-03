f3b32ac352ba80318e714a6e30ddd952503fd2cf
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2026-01-03 18:53:23 +0100

    add logging to video indexing

--- src/Scheduler/Task/VideoIndexing.php
+++ src/Scheduler/Task/VideoIndexing.php
@@ -4,6 +4,7 @@ namespace App\Scheduler\Task;
 
 use App\Service\CameraManager;
 use App\Service\VideoFileManager;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Scheduler\Attribute\AsPeriodicTask;
 
 /**
@@ -12,14 +13,16 @@ use Symfony\Component\Scheduler\Attribute\AsPeriodicTask;
 #[AsPeriodicTask(frequency: 60)]
 class VideoIndexing
 {
-    public function __construct(private readonly CameraManager $cameraManager, private readonly VideoFileManager $videoFileManager)
+    public function __construct(private readonly CameraManager $cameraManager, private readonly VideoFileManager $videoFileManager, private readonly LoggerInterface $logger)
     {
     }
 
     public function __invoke(): void
     {
+        $this->logger->info('Starte Video-Cleanup Task...');
         $cameras = $this->cameraManager->getCameras();
         foreach ($cameras as $camera) {
+            $this->logger->info('Starte indexing for camera ' . $camera->getTitle());
             //indexing is implemented in the videoFileManager, we just trigger a select to index new files. This is all we need to do.
             $this->videoFileManager->getVideos($camera);
         }
